88b00bf91a0771c144aa56774654ebf925b383a0
Chaste <b.chiastellino@gmail.com>
2024-03-25 10:02:55 -0300

    end produccion

---
+++ 03-gifsapp/src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,23 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  templateUrl: './search-box.components.html'
+
+
+
+})
+
+
+
+export class SearchBoxComponent {
+
+  @ViewChild('textTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+
+  searchTag(newTag: string): void {
+    console.log(newTag);
+  }
+
+}
