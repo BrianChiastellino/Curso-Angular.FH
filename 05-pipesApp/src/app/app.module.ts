02fb8be240d7f1c9b4036142a9767c892d292095
Brian Chaste <b.chiastellino@gmail.com>
2024-04-30 16:53:59 -0300

    end pipe angular

--- 05-pipesApp/src/app/app.module.ts
+++ 05-pipesApp/src/app/app.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
+// Configuracion de idioma de la APP
+
+import localeEsHn from '@angular/common/locales/es-HN'      //Bajamos el idioma 
+import localeFrCa from '@angular/common/locales/fr-CA'
+import { registerLocaleData } from '@angular/common'
+registerLocaleData(localeEsHn);                             //Registramos el idioma
+registerLocaleData(localeFrCa);
 
 @NgModule({
   declarations: [
@@ -17,7 +24,9 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
 
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-HN' }, //Inyectamos el idioma diciendo que va  a ser local para la app
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18n Select
+  public name: string = 'Brian';
+  public gender: 'male' | 'female' = 'male';
+
+  public invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  };
+
+  public changeClient(): void {
+    this.name = 'Candela';
+    this.gender = 'female';
+
+    setTimeout(() => {
+      this.name = 'Brian';
+      this.gender = 'male';
+    }, 2000);
+  }
+
+  // i18nPlural
+
+  public clients: string[] = ['Brian', 'Candela', 'Noah', 'Tiziano', 'Claudia', 'Gustavo'];
+
+  public clientsMap = {
+    '=0': 'no tenemos ningun clienete esperando',
+    '=1': 'tenemos un cliente esperando',
+    'other': 'tenemos # clientes esperando',
+  }
+
+
+  public deleteClient(): void {
+    this.clients.shift();
+  }
+
 }
