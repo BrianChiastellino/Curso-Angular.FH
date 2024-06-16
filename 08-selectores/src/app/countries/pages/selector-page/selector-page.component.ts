827d6af3ad2e861a2c764f676470add1971115a2
Brian Chaste <b.chiastellino@gmail.com>
2024-06-16 10:03:30 -0300

    add country app

---
+++ 08-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-selector-page',
+  templateUrl: './selector-page.component.html',
+
+})
+
+export class SelectorPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    borders: ['', Validators.required],
+
+  })
+
+  constructor(private fb: FormBuilder) { }
+
+
+
+}
