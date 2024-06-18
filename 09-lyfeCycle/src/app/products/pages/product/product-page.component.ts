17af202690830415437eeb2d2cafe1bf7db43a60
Chaste <b.chiastellino@gmail.com>
2024-06-18 18:15:05 -0300

    add lifecycle hooks

---
+++ 09-lyfeCycle/src/app/products/pages/product/product-page.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products-product-page',
+  templateUrl: './product-page.component.html',
+})
+
+export class ProductPageComponent {
+
+
+ }
